f35a6c4cd45c3f569535a1d1f9d2af122b1c7cb9
Edgar Fuentes <69331480+efuentes25@users.noreply.github.com>
2023-09-26 10:21:10 -0700

    Update StudentDTO.java

--- cst438_registration/src/main/java/com/cst438/domain/StudentDTO.java
+++ cst438_registration/src/main/java/com/cst438/domain/StudentDTO.java
@@ -1,30 +1,5 @@
 package com.cst438.domain;
 
 public record StudentDTO(int student_id, String name, String email, String status, int status_code) {
-
-	public void setStudent_id(int i) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void setName(String string) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void setStatus(String string) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void setEmail(String string) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void setStatus_code(int i) {
-		// TODO Auto-generated method stub
-		
-	}
 	
 }
